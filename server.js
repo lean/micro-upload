818bda34474662cd201ff1dccf8d91e0315f46d4
leandro cabrera <leandcabrera@gmail.com>
2020-01-28 15:26:00 -0300

    change port

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ fastify.post('/', function (req, reply) {
   }
 })
 
-fastify.listen(3000, err => {
+fastify.listen(process.env.PORT || 5000, err => {
   if (err) throw err
   console.log(`server listening on ${fastify.server.address().port}`)
 })
